463bcb3bde1f0c3448df7981429d46614e63d2b8
Maupertuisbug <researchj09@gmail.com>
2025-05-17 00:31:50 +0000

    update constrained code

--- constrained_batch_policy_learning.py
+++ constrained_batch_policy_learning.py
@@ -7,12 +7,13 @@ import numpy as np
 
 
 class CBPL:
-    def __init__(self, dataset, l1, B, learning_rate):
+    def __init__(self, dataset, l1, B, learning_rate, env):
         self.dataset = dataset 
         self.l1 = l1 
         self.bound = B 
         self.lr = learning_rate 
-        self.lmda = torch.full((1,), (B/1+1))
+        self.lmda = torch.nn.Parameter(torch.full((1,), (B/1+1)))
+        self.env = env
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # You need to initialise the dataset such that rewards and cost from constraints 
@@ -30,10 +31,29 @@ class CBPL:
 
     
     def run(self, t):
+        input_size = self.env.observation_space.n + self.env.action_space.
+        output_size = 1
+        layers = [] 
+        layers.append([input_size, 'relu', hidden_units])
+        hidden_units = 64 
+        num_layers = 1
+        for layer in range(num_layers - 1):
+            layers.append([hidden_units, 'relu', hidden_units])
+        layers.append([hidden_units, 'linear', output_size])
+        print("Layers: ", layers)
+
+        avg_q_function = Network(num_layers+1, layers).to(self.device)
+
+
+
+
+        
 
 
         for i in range(t):
 
+            # You need to prepare data for this 
+            
             fitted_q_function = FQI() # You need to pass policy that you learnt using the regulazarization 
             current_cost = FQE(fitted_q_function, self.rewards)
             current_constraint = FQE(fitted_q_function, self.constraints)
--- fqi.py
+++ fqi.py
@@ -12,33 +12,40 @@ def one_hot(n, state):
     return vec 
 
 class FQI:
-    def __init__(self, tuple_dataset, config, model, wandb=None):
+    def __init__(self, tuple_dataset, config, model, input_batch=None, target=None, dones = None, wandb=None):
         self.wandb = wandb
         self.tuple_dataset = tuple_dataset
         self.config = config
         self.model = model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
+        self.input_batch = input_batch
+        self.cost = target
+        self.dones = dones
 
     def one_hot_to_state(one_hot_vector):
         return int(np.argmax(one_hot_vector))
 
 
     def train(self):
-        # You need the dataset as torch tensors
 
-        states, actions, next_states, rewards, dones = zip(*self.tuple_dataset)
+        if self.tuple_dataset is not None:
+            # You need the dataset as torch tensors
+            states, actions, next_states, rewards, dones = zip(*self.tuple_dataset)
 
-        # You need to split the dataset into states, actions, rewards, next_states, and dones
-        states = torch.tensor(np.array(states), dtype=torch.float32, device=self.device)
-        actions = torch.tensor(np.array(actions), dtype=torch.float32, device=self.device)
-        next_states = torch.tensor(np.array(next_states), dtype=torch.float32, device=self.device)
-        rewards     = torch.tensor(np.array(rewards), dtype=torch.float32, device=self.device)
-        dones       = torch.tensor(np.array(dones), dtype=torch.float32, device=self.device)
+            # You need to split the dataset into states, actions, rewards, next_states, and dones
+            states = torch.tensor(np.array(states), dtype=torch.float32, device=self.device)
+            actions = torch.tensor(np.array(actions), dtype=torch.float32, device=self.device)
+            next_states = torch.tensor(np.array(next_states), dtype=torch.float32, device=self.device)
+            rewards     = torch.tensor(np.array(rewards), dtype=torch.float32, device=self.device)
+            dones       = torch.tensor(np.array(dones), dtype=torch.float32, device=self.device)
 
-        # You need to create the target Q-values
-        input_dataset = torch.cat((states, actions), dim=1)
-        dataset = TensorDataset(input_dataset, rewards, dones)
+            # You need to create the target Q-values
+            input_dataset = torch.cat((states, actions), dim=1)
+            dataset = TensorDataset(input_dataset, rewards, dones)
+        
+        else :
+            dataset = TensorDataset(self.input_dataset, self.cost, self.dones)
         loader = DataLoader(dataset, batch_size=128, shuffle=True)
         print("Input dataset shape: ", input_dataset.shape)
         losses = []
@@ -64,11 +71,13 @@ class FQI:
         env = gym.make('FrozenLake-v1', desc=None, is_slippery=False)
         for episodes in range(self.config.evaluation_episodes):
             state = env.reset()
+            state_n = env.observation_space.n 
+            action_n = env.action_space.n
             done = False 
             total_reward = 0
             print(episodes)
             while not done:
-                q_values = [self.model(torch.cat((torch.tensor(one_hot(state), dtype=torch.float32, device=self.device).unsqueeze(0), torch.tensor(one_hot(a), dtype=torch.float32, device=self.device).unsqueeze(0)), dim=0)).detach().cpu().numpy() for a in range(env.action_space.n)]
+                q_values = [self.model(torch.cat((torch.tensor(one_hot(state_n, state), dtype=torch.float32, device=self.device), torch.tensor(one_hot(action_n, a), dtype=torch.float32, device=self.device)), dim=0)).detach().cpu().numpy() for a in range(env.action_space.n)]
                 print(q_values)
                 action = int(np.argmax(np.argmax(q_values)))
                 next_state, reward, done, _ = env.step(action)
